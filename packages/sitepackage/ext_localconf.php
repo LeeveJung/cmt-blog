064408c5b1a62099e7e2f0d5ca5c6355b697a6b9
Christoph Brauer <brauer@faktor-e.de>
2026-06-19 15:51:00 +0200

    [TASK] adds yoast SEO analysis for the blog-page Content

--- packages/sitepackage/ext_localconf.php
+++ packages/sitepackage/ext_localconf.php
@@ -5,6 +5,8 @@ defined('TYPO3') or die('Access denied.');
 // Add default RTE configuration
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['sitepackage'] = 'EXT:sitepackage/Configuration/RTE/Default.yaml';
 
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['yoast_seo']['allowedDoktypes']['blog'] = 1775660402;
+
 // Auto-fill TOC item label from linked content element header
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][]
     = \Brauer\Sitepackage\DataHandler\TocItemLabelEnricher::class;
